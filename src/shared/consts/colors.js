3ac02e026d4e62817ca4ca4a5ae2c6ccb3a471bb
juliocabrera820 <juliocabrera820@gmail.com>
2022-02-27 14:31:48 -0600

    add base colors

---
+++ src/shared/consts/colors.js
@@ -0,0 +1,16 @@
+const colors = {
+  primary: '#3D2C8D',
+  secondary: '#916BBF',
+  terciary: '#C996CC'
+}
+
+const hoverColors = {
+  secondary: '#3D2C8D',
+  primary: '#916BBF',
+  terciary: '#916BBF'
+}
+
+export {
+  colors,
+  hoverColors
+}
