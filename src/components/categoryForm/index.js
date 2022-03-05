1dff1066f793d64a4d8745e416e8a1784caaeabd
juliocabrera820 <juliocabrera820@gmail.com>
2022-03-05 14:17:50 -0600

    add category form component

---
+++ src/components/categoryForm/index.js
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { categorySchema } from "../../schemas/categorySchema";
+import { feedsContext } from "../../providers/feeds/feedsContext";
+import { Button, FormField } from "../../shared/styles";
+
+const FeedForm = () => {
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(categorySchema),
+  });
+  const { setCurrentCategory } = useContext(feedsContext);
+
+  const onSubmit = (data, e) => {
+    addCategory(data);
+    e.target.reset();
+  };
+
+  const addCategory = ({ name }) => {
+    setCurrentCategory({ name });
+  };
+
+  return (
+    <div className="mt-5">
+      <h4>New category</h4>
+      <form onSubmit={handleSubmit(onSubmit)} className="mt-2">
+        <div className="col-md-10 py-2">
+          <FormField
+            type="text"
+            placeholder="Type name"
+            ref={register}
+            name="name"
+            className={`form-control ${errors.name ? "is-invalid" : ""}`}
+          />
+          <div className="invalid-feedback">
+            <p>{errors.name && errors.name.message}</p>
+          </div>
+        </div>
+        <Button buttonType="primary" className="btn btn-success mt-4">
+          Add category
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default FeedForm;
