f4de1c9643525ab791f7f8fc7605818df7617af4
juliocabrera820 <juliocabrera820@gmail.com>
2022-03-05 12:18:35 -0600

    create feeds

--- src/pages/feeds/index.js
+++ src/pages/feeds/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { feedsService } from "../../services/feedsService";
@@ -8,7 +8,6 @@ import { Button } from "../../shared/styles";
 import { Field } from "./styles";
 
 const Feeds = () => {
-  const [feeds, setFeeds] = useState([]);
   const [currentFeeds, setCurrentFeeds] = useState([]);
   const { register, handleSubmit, errors } = useForm({
     resolver: yupResolver(feedSchema),
@@ -23,62 +22,70 @@ const Feeds = () => {
     setCurrentFeeds([...currentFeeds, { url, category }]);
   };
 
-  useEffect(() => {
-    retrieveFeeds();
-  }, []);
-
-  const retrieveFeeds = async () => {
-    const { data } = await feedsService().all();
-    setFeeds(data);
+  const sendFeeds = async () => {
+    const { data } = await feedsService().create(currentFeeds)
+    console.log(data)
   };
 
   return (
-    <div className="container col-md-6 mt-5">
-      <BackHome />
-      <form onSubmit={handleSubmit(onSubmit)} className="mt-5">
-        <div className="col-md-10 py-2">
-          <Field
-            type="text"
-            placeholder="Type url"
-            ref={register}
-            name="url"
-            className={`form-control ${errors.url ? "is-invalid" : ""}`}
-          />
-          <div className="invalid-feedback">
-            <p>{errors.url && errors.url.message}</p>
-          </div>
+    <div className="container col-md-9 mt-5">
+      <div className="row">
+        <div className="col-8">
+          <BackHome />
+          <form onSubmit={handleSubmit(onSubmit)} className="mt-5">
+            <div className="col-md-10 py-2">
+              <Field
+                type="text"
+                placeholder="Type url"
+                ref={register}
+                name="url"
+                className={`form-control ${errors.url ? "is-invalid" : ""}`}
+              />
+              <div className="invalid-feedback">
+                <p>{errors.url && errors.url.message}</p>
+              </div>
+            </div>
+            <div className="col-md-10 py-2">
+              <Field
+                type="text"
+                placeholder="Type category"
+                ref={register}
+                name="category"
+                className={`form-control ${
+                  errors.category ? "is-invalid" : ""
+                }`}
+              />
+              <div className="invalid-feedback">
+                <p>{errors.category && errors.category.message}</p>
+              </div>
+            </div>
+            <Button buttonType="primary" className="btn btn-success mt-4">
+              Add feed
+            </Button>
+          </form>
         </div>
-        <div className="col-md-10 py-2">
-          <Field
-            type="text"
-            placeholder="Type category"
-            ref={register}
-            name="category"
-            className={`form-control ${errors.category ? "is-invalid" : ""}`}
-          />
-          <div className="invalid-feedback">
-            <p>{errors.category && errors.category.message}</p>
-          </div>
-        </div>
-        <Button buttonType="primary" className="btn btn-success">
-          Add feed
-        </Button>
-      </form>
-      <h3>Feeds saved</h3>
-      <ul className="mt-5">
-        {feeds?.map(({ name }) => (
-          <li>{name}</li>
-        ))}
-      </ul>
-      <h3>Current feeds</h3>
-      <ul className="mt-5">
-        {currentFeeds?.map(({ url, category }) => (
+        <div className="col-4 d-flex justify-content-between">
           <div>
-            <li>{url}</li>
-            <li>{category}</li>
+            <h4>Current feeds</h4>
+            <ul className="mt-5">
+              {currentFeeds?.map(({ url }) => (
+                <div>
+                  <li>{url}</li>
+                </div>
+              ))}
+            </ul>
           </div>
-        ))}
-      </ul>
+          <div>
+            <Button
+              buttonType="secondary"
+              className="btn btn-success"
+              onClick={sendFeeds}
+            >
+              Send feeds
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
